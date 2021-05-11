e1e7973aca1009f2fde75dfe8f97ff393bfe42e6
Moe Bourji <mrbourji@gmail.com>
2021-05-11 12:15:34 +0100

    Added Ticker endpoint response models, removed summary from equities model (mirroring db)

--- backend/app/models/security.py
+++ backend/app/models/security.py
@@ -17,28 +17,36 @@ class SecurityBase(CoreModel):
     """
 
 
-class SecurityCreate(SecurityBase):
-    tickers: List[str]
-
-
-class SecurityUpdate(SecurityBase):
-    pass
-
-class SecurityInDB(IDModelMixin, SecurityBase):
+class SecurityInDB(SecurityBase):
+    """Instances of securities registered in database."""
     ticker: str
     type: str
 
-class SecurityPublic(IDModelMixin, SecurityBase):
-    pass
+class SecuritiesAddedToDB(CoreModel):
+    """Contains list of SecurityInDB models that were added to DB."""
+    message: str = "The following tickers have been successfully posted to the database:"
+    securities: List[SecurityInDB]
 
+class InvalidTickers(CoreModel):
+    """List of invalid tickers that were not added to DB."""
+    message: str = "The following tickers are invalid and have not been posted to the database:"
+    tickers: List[str]
 
+class SecuritiesAlreadyInDB(CoreModel):
+    """Contains list of SecurityInDB models that were already added to DB and cannot be re-added."""
+    message: str = "The following tickers were already included in the database and cannot be re-added:"
+    securities: List[SecurityInDB]
 
+class POSTTickerResponse(CoreModel):
+    """Response model for POST ticker endpoint"""
+    added_tickers: SecuritiesAddedToDB
+    existing_tickers: SecuritiesAlreadyInDB
+    invalid_tickers: InvalidTickers
 
 class EquityCreate(SecurityBase):
     ticker: str
     name: str
     country: str
-    summary: str
     sector: str
     industry: str
     exchange: str
@@ -48,15 +56,14 @@ class EquityUpdate(SecurityBase):
     pass
 
 
-class EquityInDB(IDModelMixin, SecurityBase):
+class EquityInDB(SecurityBase):
     ticker: str
     name: str
     country: str
-    summary: str
     sector: str
     industry: str
     exchange: str
 
 
-class EquityPublic(IDModelMixin, SecurityBase):
+class EquityPublic(SecurityBase):
     pass
