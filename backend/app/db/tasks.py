b073421c2091f59bf5ffa8eb022d7f61bb4414ea
Moe Bourji <mrbourji@gmail.com>
2021-06-16 15:35:29 +0100

    apply migrations on startup

--- backend/app/db/tasks.py
+++ backend/app/db/tasks.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from databases import Database
 from app.core.config import DATABASE_URL
 import logging
+import warnings
+from alembic.config import Config
+import alembic
 
 logger = logging.getLogger(__name__)
 
@@ -26,3 +29,11 @@ async def close_db_connection(app: FastAPI) -> None:
         logger.warn("--- DB DISCONNECT ERROR ---")
         logger.warn(e)
         logger.warn("--- DB DISCONNECT ERROR ---")
+
+def apply_migrations():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    os.environ["TESTING"] = "0"
+    config = Config("alembic.ini")
+    alembic.command.upgrade(config, "head")
+    yield
+    alembic.command.downgrade(config, "base")
